b834914782531ff56cf6158107193ee7bffbc587
Ozal14 <lider.ovm@gmail.com>
2021-10-03 12:41:17 -0400

    update second file again

--- src/First/Second.java
+++ src/First/Second.java
@@ -3,14 +3,14 @@ package First;
 public class Second {
     public void m(){
 
-
+        System.out.println ("first method");
     }
 
     public void m(int a){
-
+        System.out.println ("second method");
     }
 
     public void m(String s){
-
+        System.out.println ("tir method");
     }
 }
