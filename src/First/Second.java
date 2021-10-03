fdeff0df87e706dd1d8fb1b8fc2b5356ec836f75
Ozal14 <lider.ovm@gmail.com>
2021-10-03 13:07:21 -0400

    update second file again

--- src/First/Second.java
+++ src/First/Second.java
@@ -3,15 +3,15 @@ package First;
 public class Second {
     public void m(){
 
-        System.out.println ("first method");
+
     }
 
     public void m(int a){
-        System.out.println ("second method");
+
     }
 
     public void m(String s){
-        System.out.println ("tir method");
+
     }
 
     public void d(double t){
